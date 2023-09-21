9e1eacb97976e34c73a9ef7e7d70607244ab09bb
trecer <qdz91634@gmail.com>
2023-09-21 13:50:33 +0300

    modified:   .env modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup
 from aiogram.utils import executor
 from dotenv import load_dotenv
 import os
@@ -7,10 +8,53 @@ load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Каталог').add('Корзина').add('Контакты')
+
+main_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+main_admin.add('Каталог').add('Корзина').add('Контакты').add('Админ-панель')
+
+admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
+admin_panel.add('Добавить товар').add('Удалить товар').add('Сделать рассылку')
+
+
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer(f'{message.from_user.first_name}, Добро пожаловать в магазин кроссовок!')
+    await message.answer_sticker('CAACAgIAAxkBAAMNZQwXWT6dILmr7PmsUtDZF2mL93EAAgUAA8A2TxP5al-agmtNdTAE')
+    await message.answer(f'{message.from_user.first_name}, Добро пожаловать в магазин кроссовок!',
+                         reply_markup=main)
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Вы авторизировались как администратор',
+                             reply_markup=main_admin)
+
+
+@dp.message_handler(commands=['id'])
+async def cmd_id(message: types.Message):
+    await message.answer(f'{message.from_user.id}')
+
+
+@dp.message_handler(text='Каталог')
+async def catalog(message: types.Message):
+    await message.answer(f'Каталог пуст!')
+
+
+@dp.message_handler(text='Корзина')
+async def cart(message: types.Message):
+    await message.answer(f'Корзина пуста!')
+
+
+@dp.message_handler(text='Контакты')
+async def contacts(message: types.Message):
+    await message.answer(f'Покупать товары у него: @Urii_Logachev')
+
+
+@dp.message_handler(text='Админ-панель')
+async def open_admin_panel(message: types.Message):
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Вы вошли в админ-панель', reply_markup=admin_panel)
+    else:
+        await message.reply('Я тебя не понимаю.')
 
 
 @dp.message_handler()
@@ -18,5 +62,7 @@ async def answer(message: types.Message):
     await message.reply('Я тебя не понимаю.')
 
 
+
 if __name__ == '__main__':
     executor.start_polling(dp)
+
