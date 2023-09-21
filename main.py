11485bdc958bd6dcb83872897285f7dcbccc2d69
trecer <qdz91634@gmail.com>
2023-09-21 13:06:02 +0300

    new file:   .env new file:   main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f'{message.from_user.first_name}, Добро пожаловать в магазин кроссовок!')
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply('Я тебя не понимаю.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
